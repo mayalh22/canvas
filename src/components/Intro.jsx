9ed37a389ac139e5828397ebdba48344d4ed9aea
mayalh22 <maya.l.hazarika@gmail.com>
2026-03-19 16:37:34 -0400

    add all component jsx

---
+++ src/components/Intro.jsx
@@ -0,0 +1,8 @@
+export default function Intro() {
+  return (
+    <section>
+      <h1 className="title">Canvas</h1>
+      <p className="subtitle">Code something worth looking at.</p>
+    </section>
+  )
+}
